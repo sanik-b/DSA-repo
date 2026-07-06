b8babe59f59516aff2c354afd1d888e1102b45fd
sanik-b <sanikababar5002@gmail.com>
2026-07-06 11:50:51 +0530

    Time: 6 ms (22.46%) | Memory: 46.1 MB (33.94%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0){
+            return false;
+        }
+        String str = Integer.toString(x);
+        int left = 0;
+        int right = str.length() -1;
+        while(left<right){
+            if (str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
